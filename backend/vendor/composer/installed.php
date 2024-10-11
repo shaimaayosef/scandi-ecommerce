cf6d4dca43a6ed18697ec11d199390e9adfe9b9d
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-10-11 15:27:33 +0300

    start backend with provided carcass and install dependencies

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'scandiweb/fullstack',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '22b22bfb5e3f1a97ddc6c95b05d714695356181c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nikic/fast-route' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '181d480e08d9476e61381e04a71b34dc0432e812',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nikic/fast-route',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'scandiweb/fullstack' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '22b22bfb5e3f1a97ddc6c95b05d714695356181c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webonyx/graphql-php' => array(
+            'pretty_version' => 'v15.2.1',
+            'version' => '15.2.1.0',
+            'reference' => '6b02da9313065889eb783d623567476500a572ef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webonyx/graphql-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
